e05f140169cb14b1491cb6a985eca5b09b1fc626
Matthew Wendzina <mattwendzina@gmail.com>
2020-06-26 14:33:08 +0100

    Gatsby clean

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import React from "react"
-
-// import { Link } from "gatsby"
-
 import Layout from "../components/Layout"
 import Head from "../components/Head"
 import SimpleHero from "../components/SimpleHero"
